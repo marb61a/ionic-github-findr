d7c8b0357ba2e98188d3464e3e9ca6433e86ca4d
martin byrne <marb61a@gmail.com>
2017-10-22 09:45:58 +0000

    added search results componennt and components module

---
+++ src/components/search-results/search-results.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SearchResults } from './search-results';
+
+@NgModule({
+    declarations: [
+        SearchResults,
+    ],
+    imports: [
+        IonicPageModule.forChild(SearchResults),    
+    ],
+    exports: [
+        SearchResults    
+    ]
+})
+export class SearchResultsModule{
+    
+}
